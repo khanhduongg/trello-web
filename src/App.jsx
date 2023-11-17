5c15ade68c7aeefc29cf9a1c29dc32b2bda66cc7
khanhduongg <khanhduong04.199@gmail.com>
2023-11-17 11:12:07 +0700

    dark_light_mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import {useColorScheme} from '@mui/material/styles'
+import Typoraphy from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
-import Typoraphy from'@mui/material/Typography'
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
 
   return (
     <>
+      <ModeToggle/>
+      <hr/>
       <Typoraphy variant='body2' color="text.secondary">Test Typoraphy</Typoraphy>
+      <Button variant="contained">Hello world</Button>
     </>
   )
 }
